1707bd113d9158c79592f1787882990ca7ee6d83
David Cely <dacely@gmail.com>
2020-11-03 11:55:01 -0500

    Pass by reference

--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace Liang_Ch08
             //L01_PassTwoDimensionalArray.TestJagged();
             //L01_PassTwoDimensionalArray.TestRectangular();
             //L02_GradeExam.Execute();
-            L03_FindNearestPoints.Execute();
+            //L03_FindNearestPoints.Execute();
+            T02_PassByReference.M1();
         }
     }
 }
---
+++ T01_PassByReference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Liang_Ch08
+{
+    class T01_PassByReference
+    {
+        public static void M1()
+        {
+            int x = 1;
+            Console.WriteLine("M1: x = {0}", x);
+            M2(ref x);
+            Console.WriteLine("M1: x = {0}", x);
+        }
+
+        private static void M2(ref int y)
+        {
+            y = 2;
+            Console.WriteLine("M2: y = {0}", y);
+            M3(ref y);
+            Console.WriteLine("M2: y = {0}", y);
+        }
+
+        private static void M3(ref int z)
+        {
+            z = 3;
+            Console.WriteLine("M3: z = {0}", z);
+        }
+    }
+}
